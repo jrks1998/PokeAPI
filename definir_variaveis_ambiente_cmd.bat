ba73ea30e51babba435218139889463040d43432
Jonas Schneider <klicheti@gmail.com>
2025-08-18 21:30:28 -0300

    atualizando definir_variaveis_ambiente_cmd.bat

--- definir_variaveis_ambiente_cmd.bat
+++ definir_variaveis_ambiente_cmd.bat
@@ -3,3 +3,4 @@
 set /p DB_USER_POKEAPI=informe o usuario do banco de dados:
 set /p DB_PASS_POKEAPI=informe a senha do banco de dados:
 set /p DB_NAME_POKEAPI=informe o nome do banco de dados:
+set /p DB_PORT_POKEAPI=informe a porta do banco de dados (padrão 5432):
